c0d20f5769a4d59b70ef70c21abfcb366506f575
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2026-01-26 15:26:47 -0500

    create the story teller

---
+++ Assets/Scripts/Storyteller.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+//this script is responsible for handling the telling of the story
+public class Storyteller : MonoBehaviour
+{
+    private int _currentLineNum;
+    private int _nextLineNum;
+    private string _nextLineText;
+    private GameObject _targetTextObject;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadNextLine()
+    {
+        
+    }
+}
