fe704bd85026e9e24d1efd79be8553b23731d650
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2026-02-27 08:54:20 -0500

    essay dialogue

--- Assets/Scripts/Listener Scripts/Essay Listener.cs
+++ Assets/Scripts/Listener Scripts/Essay Listener.cs
@@ -1,19 +1,35 @@
+using Dialogue;
+using UnityEditor.Events;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class EssayListener : MonoBehaviour
 {
     [Header("Dialogue")]
     [SerializeField] private GameObject DialogueManager;
     [SerializeField] private GameObject ActManager;
-    
+    [SerializeField] private Story EssayStory;
+
     DialogueSystem dialogueSystem;
-    ActDirector actDirector;
+    ActDirector actDirector;   
     
+    [Header("Relevant Game Objects")] 
+    [SerializeField] private GameObject TransitionScreen;
+    [SerializeField] private GameObject EssayText;
+    [SerializeField] private GameObject ContinueButton;
+    [SerializeField] private GameObject MichaelsoftWord;
+    private Button continueButton;
+    private Button michaelsoftWordButton;
+
+    private bool hasDialogueBeenSeen = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         dialogueSystem = DialogueManager.GetComponent<DialogueSystem>();
         actDirector = ActManager.GetComponent<ActDirector>();
+        continueButton = ContinueButton.GetComponent<Button>();
+        michaelsoftWordButton = MichaelsoftWord.GetComponent<Button>();
     }
 
     // Update is called once per frame
@@ -26,6 +42,10 @@ public class EssayListener : MonoBehaviour
     {
         dialogueSystem.DialogueImpactfulChoiceEvent.AddListener(WriteEmail);
         dialogueSystem.DialogueEndEvent.AddListener(MarkComplete);
+        if (hasDialogueBeenSeen == false)
+        {
+            EssayDialogue();
+        }
     }
 
     public void RemoveListeners()
@@ -39,16 +59,29 @@ public class EssayListener : MonoBehaviour
         if (isManual == 1)
         {
             //script for manually writing the essay
+            continueButton.onClick.AddListener(transitionScreen);
         }
         else
         {
             // script for LYBL writing the essay
+            EssayText.SetActive(true);
         }
     }
 
+    void transitionScreen()
+    {
+        TransitionScreen.SetActive(true);   
+        continueButton.onClick.RemoveListener(transitionScreen);
+    }
+    
     void MarkComplete()
     {
-        actDirector.MarkTaskAsDone("Essay");
+        hasDialogueBeenSeen = true; // set this so that dialogue doesn't trigger again
         RemoveListeners();
+    }
+
+    void EssayDialogue()
+    {
+        dialogueSystem.StartDialogue(EssayStory);
     }
 }
