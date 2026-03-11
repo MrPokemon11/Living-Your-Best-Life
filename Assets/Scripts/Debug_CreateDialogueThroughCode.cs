d96543abdf194857a043a8fa3a853a087329d3b3
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2026-03-11 09:56:03 -0400

    add more dialogue

--- Assets/Scripts/Debug_CreateDialogueThroughCode.cs
+++ Assets/Scripts/Debug_CreateDialogueThroughCode.cs
@@ -15,7 +15,7 @@ public class Debug_CreateDialogueThroughCode : MonoBehaviour
     void Start()
     {
        story = ScriptableObject.CreateInstance<Story>();
-       story.branching = new BranchingDialogue[25];
+       story.branching = new BranchingDialogue[18];
        for (int i = 0; i < story.branching.Length; i++)
        {
            story.branching[i] = new BranchingDialogue();
@@ -42,116 +42,28 @@ public class Debug_CreateDialogueThroughCode : MonoBehaviour
         //dialogue line template
         //branching paths must be fixed manually, but it's easier than doing the whole thing in the inspector!
         /**
-        dialogueQs.Add("Click to continue)");
+        dialogueQs.Add("(Click to continue)");
         dialogueResponses.Add("");
-        dialogueNextSteps.Add(1)
+        dialogueNextSteps.Add(1);
         **/
         
-        // story elements go here
+        // names go here
         //in branching dialogue, the playerName is used as the first line of text
-        story._playerName = "Say cheese!";
+        story._playerName = "Really? Another essay?";
         story._NPCName = "LYBL";
-
-        //add the lines
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("You're looking, um, good..?");
+        
+        // add the lines
+        dialogueQs.Add("It's for my Latin Language class.");
+        dialogueResponses.Add("Fine. Let's see what the requirements are...");
         dialogueNextSteps.Add(1);
         
         dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("But we can do better! Just give me a moment...");
+        dialogueResponses.Add("111 pages?!");
         dialogueNextSteps.Add(2);
         
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("Here! How does this look?");
+        dialogueQs.Add("Those are roman numerals, Lybl.");
+        dialogueResponses.Add("This is outrageous!");
         dialogueNextSteps.Add(3);
-        
-        dialogueQs.Add("Who's the woman?");
-        dialogueResponses.Add("It's your girlfriend! Isn't she beautiful?");
-        dialogueNextSteps.Add(4);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("Anyway the image looks good right? I can send it to her?");
-        dialogueNextSteps.Add(5);
-        
-        //branch
-        dialogueQs.Add("Looks good to me.");
-        dialogueResponses.Add("Oh good, I was a little worried...");
-        dialogueNextSteps.Add(6);
-        dialogueQs.Add("That's not my girlfriend...");
-        dialogueResponses.Add("Oh.");
-        dialogueNextSteps.Add(16);
-        
-        dialogueQs.Add("Why would you be worried?");
-        dialogueResponses.Add("I wasn't entirely sure...");
-        dialogueNextSteps.Add(7);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("Anyway, we can cross it off the list!");
-        dialogueNextSteps.Add(8);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("Now, what else do you-");
-        dialogueNextSteps.Add(9);
-        
-        dialogueQs.Add("What's wrong?");
-        dialogueResponses.Add("So um... turns out that wasn't her in the photo.");
-        dialogueNextSteps.Add(10);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("And she wants to know who the other woman is.");
-        dialogueNextSteps.Add(11);
-        
-        dialogueQs.Add("You've got to be kidding me... let me talk to her.");
-        dialogueResponses.Add("No, it's fine! I'm handling it-");
-        dialogueNextSteps.Add(12);
-        
-        dialogueQs.Add("Lybl...");
-        dialogueResponses.Add("I've um. Made everything worse.");
-        dialogueNextSteps.Add(13);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("She says it's over between the two of you.");
-        dialogueNextSteps.Add(14);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("... so I may have messed this up, but you still have things I can help with right?");
-        dialogueNextSteps.Add(15);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("Let's go do those!");
-        dialogueNextSteps.Add(-1);
-        
-        dialogueQs.Add("What do you mean, \"Oh\"?");
-        dialogueResponses.Add("I already sent the photo to her.");
-        dialogueNextSteps.Add(17);
-        
-        dialogueQs.Add("What? Why?");
-        dialogueResponses.Add("You said I could!");
-        dialogueNextSteps.Add(18);
-        
-        dialogueQs.Add("No I didn't?");
-        dialogueResponses.Add("Y-you did though! You said \"I can send it to her\"!");
-        dialogueNextSteps.Add(19);
-        
-        dialogueQs.Add("You said that.");
-        dialogueResponses.Add("Oh. Oh no.");
-        dialogueNextSteps.Add(20);
-        
-        dialogueQs.Add("What now?");
-        dialogueResponses.Add("She... she wants to break up.");
-        dialogueNextSteps.Add(21);
-        
-        dialogueQs.Add("You're joking. Let me talk to her.");
-        dialogueResponses.Add("No no, I can handle this!");
-        dialogueNextSteps.Add(22);
-        
-        dialogueQs.Add("(Click to continue)");
-        dialogueResponses.Add("That's why I'm here, right? To help you Live Your Best Life(tm)?");
-        dialogueNextSteps.Add(23);
-        
-        dialogueQs.Add("...");
-        dialogueResponses.Add("Anyway... anything else I can help with?");
-        dialogueNextSteps.Add(-1);
         
         // add elements to the story
         for (int i = 0; i < dialogueQs.Count; i++)
@@ -162,7 +74,7 @@ public class Debug_CreateDialogueThroughCode : MonoBehaviour
         }
         
         // create the asset
-        string path = "Assets/Dialogue/story.asset";
+        string path = "Assets/Dialogue/Act 2/Bad Path/Essay/Act2EssayBad.asset";
         UnityEditor.AssetDatabase.CreateAsset(story, path);
         
     }
